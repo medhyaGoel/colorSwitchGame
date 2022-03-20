611564632df68e4cfd0142442304059d2e84f58c
medhyaGoel <71662879+medhyaGoel@users.noreply.github.com>
2022-03-20 11:58:42 -0700

    Add files via upload

---
+++ ColorSwitchGameWindowsFormApp/ColorSwitchGameForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace ColorSwitchGameWindowsFormApp
+{
+    partial class ColorSwitchGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.playerBox = new System.Windows.Forms.PictureBox();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.topScoresLabel = new System.Windows.Forms.Label();
+            this.topScoreTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.timerGame = new System.Windows.Forms.Timer(this.components);
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel.Controls.Add(this.obstacle2);
+            this.panel.Controls.Add(this.obstacle1);
+            this.panel.Controls.Add(this.playerBox);
+            this.panel.Location = new System.Drawing.Point(0, 1);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(200, 456);
+            this.panel.TabIndex = 0;
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.BackColor = System.Drawing.Color.Red;
+            this.obstacle2.Location = new System.Drawing.Point(0, 0);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(200, 23);
+            this.obstacle2.TabIndex = 2;
+            this.obstacle2.TabStop = false;
+            this.obstacle2.Tag = "obstacle2";
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.BackColor = System.Drawing.Color.Blue;
+            this.obstacle1.Location = new System.Drawing.Point(0, 143);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(200, 23);
+            this.obstacle1.TabIndex = 1;
+            this.obstacle1.TabStop = false;
+            this.obstacle1.Tag = "obstacle1";
+            // 
+            // playerBox
+            // 
+            this.playerBox.BackColor = System.Drawing.Color.AliceBlue;
+            this.playerBox.Location = new System.Drawing.Point(60, 349);
+            this.playerBox.Name = "playerBox";
+            this.playerBox.Size = new System.Drawing.Size(100, 26);
+            this.playerBox.TabIndex = 0;
+            this.playerBox.TabStop = false;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(236, 27);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(70, 13);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "Your score: 0";
+            // 
+            // topScoresLabel
+            // 
+            this.topScoresLabel.AutoSize = true;
+            this.topScoresLabel.Location = new System.Drawing.Point(236, 71);
+            this.topScoresLabel.Name = "topScoresLabel";
+            this.topScoresLabel.Size = new System.Drawing.Size(78, 13);
+            this.topScoresLabel.TabIndex = 2;
+            this.topScoresLabel.Text = "Top 10 scores:";
+            // 
+            // topScoreTextBox
+            // 
+            this.topScoreTextBox.Location = new System.Drawing.Point(239, 99);
+            this.topScoreTextBox.Multiline = true;
+            this.topScoreTextBox.Name = "topScoreTextBox";
+            this.topScoreTextBox.ReadOnly = true;
+            this.topScoreTextBox.Size = new System.Drawing.Size(100, 204);
+            this.topScoreTextBox.TabIndex = 3;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(239, 326);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start/restart";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(239, 364);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // timerGame
+            // 
+            this.timerGame.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ColorSwitchGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 456);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.topScoreTextBox);
+            this.Controls.Add(this.topScoresLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.panel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ColorSwitchGameForm";
+            this.Text = "Color Switch Game";
+            this.Load += new System.EventHandler(this.ColorSwitchGameForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ColorSwitchGameForm_KeyDown);
+            this.panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.PictureBox playerBox;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label topScoresLabel;
+        private System.Windows.Forms.TextBox topScoreTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Timer timerGame;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.PictureBox obstacle1;
+    }
+}
+
---
+++ ColorSwitchGameWindowsFormApp/ColorSwitchGameForm.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorSwitchGameWindowsFormApp
+{
+    public partial class ColorSwitchGameForm : Form
+    {
+        string name = "";
+        int colorID = 0;
+        Random r = new Random();
+        int time = 0;
+        int[] highestScores = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+        String[] initials = {null, null, null, null, null, null, null, null, null, null};
+        int score = 0;
+        public Boolean _isRunning = false;
+        Color[] colors = { Color.Blue, Color.Green, Color.Red, Color.Purple, Color.Yellow };
+        String HIGH_SCORE_FULL_PATH;
+        public ColorSwitchGameForm()
+        {
+            InitializeComponent();
+            // initialize the path to the high scores file
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            appDataPath = System.IO.Path.Combine(appDataPath, "ColorSwitchGame");
+            System.IO.Directory.CreateDirectory(appDataPath);
+            HIGH_SCORE_FULL_PATH = System.IO.Path.Combine(appDataPath, "HighScores.txt");
+            LoadHighScores();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //random colors
+            time = 0;
+            foreach (Control c in this.Controls)
+            {
+                if (c.Tag != null && c.Tag.ToString().StartsWith("obstacle"))
+                {
+                    //random color assignment;
+
+                    if (c.Tag.ToString().Equals("obstacle1"))
+                    {
+                        c.Top = 0;
+                        c.BackColor = colors[r.Next(0, 4)];
+                    }
+                    else if (c.Tag.ToString().Equals("obstacle2"))
+                    {
+                        c.Top = -20;
+                        c.BackColor = colors[r.Next(0, 4)];
+                    }
+                }
+            }
+            //start/reset timer
+            timerGame.Start();
+            _isRunning = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_isRunning) {
+                time += 2;
+                //when started, move boxes
+                foreach(Control c in this.panel.Controls)
+                {
+                    if(c.Tag != null && c.Tag.ToString().StartsWith("obstacle"))
+                    {
+                        c.Top += 10;
+                        if (c.Bottom >= 360)
+                        {
+                            c.Top = 0;
+                            //random color assignment;
+                            c.BackColor = colors[r.Next(0, 4)];
+                        }
+                    }
+                }
+                //if box touches player box and colors match
+                if (playerBox.Bounds.IntersectsWith(obstacle1.Bounds) && playerBox.BackColor.Equals(obstacle1.BackColor))
+                    score++;
+                else if (playerBox.Bounds.IntersectsWith(obstacle2.Bounds) && playerBox.BackColor.Equals(obstacle2.BackColor))
+                    score++;
+                else if (playerBox.Bounds.IntersectsWith(obstacle1.Bounds) || playerBox.Bounds.IntersectsWith(obstacle2.Bounds)) {
+                  //end timer
+                    timerGame.Stop();
+                    _isRunning = false;
+                    //update leaderboard text file
+                    WriteHighScores(score);
+                    //refresh leaderboard in game
+                    LoadHighScores();
+                    score = 0;
+                }
+                 scoreLabel.Text = "Your score is: " + score.ToString();
+            }
+        }
+
+        public static string GetInitials(string title, string text)
+        {
+            Form prompt = new Form()
+            {
+                Width = 300,
+                Height = 140,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = title,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 10, Top = 10, Text = text, Width = 250 };
+            TextBox textBox = new TextBox() { Left = 10, Top = 30, Width = 250 };
+            Button okButton = new Button()
+            {
+                Text = "OK",
+                Left = 10,
+                Width = 50,
+                Top = 60,
+                DialogResult = DialogResult.OK
+            };
+            okButton.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(okButton);
+            prompt.Controls.Add(textLabel);
+            prompt.MaximizeBox = false;
+            prompt.MinimizeBox = false;
+            prompt.AcceptButton = okButton;
+            prompt.ShowDialog();
+
+            return textBox.Text.Trim();
+        }
+
+        private void ColorSwitchGameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_isRunning)
+            {
+                if(e.KeyCode == Keys.Space)
+                {
+                    colorID++;
+                    if (colorID == colors.Length)
+                        colorID = 0;
+                    playerBox.BackColor = colors[colorID];
+                }
+            }
+        }
+
+        private void ColorSwitchGameForm_Load(object sender, EventArgs e)
+        {
+            LoadHighScores();
+        }
+
+        private void LoadHighScores()
+        {
+            topScoreTextBox.Text = "";
+            // load up the high scores
+            try
+            {
+                //creates reader
+                StreamReader reader = new StreamReader(HIGH_SCORE_FULL_PATH);
+                using (reader)
+                {
+                    //reads first line
+                    string line = reader.ReadLine();
+                    int indexer = 0;
+                    while (line != null)
+                    {
+
+                        // process the current line
+                        string[] parts = line.Split(',');
+                        string initial = parts[0];
+                        initials[indexer] = initial;
+                        string scoreA = parts[1];
+                        int x;
+                            int.TryParse(scoreA, out x);
+                        highestScores[indexer] = x;
+                        // append the formatted high score to the textBox
+                        topScoreTextBox.AppendText($"{initial} {scoreA,6}\r\n");
+                        indexer++;
+                        // read the next line
+                        line = reader.ReadLine();
+
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                // do nothing... no high scores yet!
+            }
+            catch (IOException ex)
+            {
+                scoreLabel.Text = ("Exception " + ex.Message);
+            }
+        }
+
+        //write in file
+        public void WriteHighScores(int score)
+        {
+            LoadHighScores();
+            //find point in [highest scores] at which to insert high score
+            for(int i = 0; i<10; i++)
+            {
+                //if score is good enough for leader board
+                if (score > highestScores[i])
+                {
+                    //ask for username
+                    name = GetInitials("New" + " High Score!", "Enter your initials:");
+                    //insert score in array of scores + name in array of initials
+                    for(int k = 9; k>i; k--)
+                    {
+                        highestScores[k] = highestScores[k - 1];
+                        initials[k] = initials[k - 1];
+                    }
+                    highestScores[i] = score;
+                    initials[i] = name;
+                    //rewrite entire text file
+                    try
+                    {
+                        //creates writer
+                        StreamWriter writer = new StreamWriter(HIGH_SCORE_FULL_PATH);
+                        using (writer)
+                        {
+                            for (int j = 0; j < 10; j++)
+                            {
+                                if (highestScores[j] != -1)
+                                {
+                                    writer.WriteLine(initials[j] + "," + highestScores[j].ToString());
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        scoreLabel.Text = ("Exception " + ex.Message);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
